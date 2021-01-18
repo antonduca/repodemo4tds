9cd8495a0e3e9b4f151da7cac59f26d1157b5604
Anton Duca <anton.duca@upb.ro>
2021-01-18 17:33:37 +0200

    Am adaugat SalutLumeV2 pentru demo!!!

---
+++ SalutLumeV2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class SalutLumeV2 {
+    public static void main(String[] args) {
+        Date d = new Date();
+        System.out.println("SalutLumeV2 - creat in repository local!");
+        System.out.println("Data si ora: " + d);
+    }
+}
